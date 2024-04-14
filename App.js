6a2794863280caf21aca4c4dc413bcde75546c4c
surya.b <surya.b@zohocorp.com>
2024-04-14 15:43:47 +0530

    second learn

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const reactElementHeading = React.createElement("div", { id: "surya" }, [
-  React.createElement("h1", { id: "surya-child1" }, "fsa"),
-  React.createElement("h1", { id: "surya-child2" }, "fsa"),
+  React.createElement("h1", { id: "surya-child1" }, "this is react"),
+  React.createElement("h1", { id: "surya-child2" }, "this is react 2"),
 ]);
 const actualRootElement = ReactDOM.createRoot(document.getElementById("root"));
 actualRootElement.render(reactElementHeading);
